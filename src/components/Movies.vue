760f82a53984deb7d6491b586194064b72be4533
AngelLeija0 <angelgaelleija2@gmail.com>
2025-06-26 15:59:44 -0500

    Adding loading

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,15 +1,22 @@
 <template>
     <section id="search" class="w-full flex flex-col">
         <div class="w-full h-full max-w-100 mx-auto mb-20 flex justify-center select-none">
-            <input @input="onInput" placeholder="Search a movie"
+            <input v-model="searchQuery" @input="onInput" placeholder="Search a movie"
                 class="text-sm w-full px-4 py-3 rounded-lg background-blur focus:border-none" autocomplete="off" />
         </div>
-        <div v-if="!loading" class="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            <MovieCard v-for="(movie, i) in moviesList" :movie="movie" :key="movie.id" @select="openMovieModal" />
+        <div v-if="!loading" class="flex flex-col w-full gap-8">
+            <div class="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                <MovieCard v-for="(movie, i) in moviesList" :movie="movie" :key="movie.id" @select="openMovieModal" />
+            </div>
+            <div class="col-span-4 flex justify-center">
+                <button type="button"
+                    class="bg-black dark:bg-white dark:hover:bg-zinc-200 px-8 py-2.5 cursor-pointer text-base rounded-lg text-white dark:text-black font-medium select-none disabled:opacity-50 disabled:cursor-not-allowed"
+                    @click="incrementMoviesQuantity" :disabled="moviesQuantity >= 100000">Load more</button>
+            </div>
         </div>
         <div v-else class="w-full flex flex-col items-center min-h-80 py-10">
-            <svg class="animate-spin [animation-duration:1.5s] size-10" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <svg class="animate-spin [animation-duration:1.5s] size-10" width="24" height="24" viewBox="0 0 24 24"
+                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M12 6l0 -3" />
                 <path d="M16.25 7.75l2.15 -2.15" />
@@ -22,8 +29,6 @@
             </svg>
             <p class="text-sm text-zinc-800 dark:text-zinc-200 mt-8">Loading movies</p>
         </div>
-
-
         <MovieModal :movie="selectedMovie" @close="selectedMovie = null" />
     </section>
 </template>
@@ -42,35 +47,52 @@ export default {
     setup() {
         const loading = ref(true);
         const moviesList = ref([]);
+        const moviesQuantity = ref(40);
         const selectedMovie = ref(null);
+        const searchQuery = ref("");
 
         const getMovies = async () => {
-            loading.value = true;
-            const response = await fetch(`${window.location.origin}/api/movies.json?pageSize=80`);
+            if (moviesList.lenght == 0) loading.value = true;
+            const response = await fetch(`${window.location.origin}/api/movies.json?pageSize=${moviesQuantity.value}`);
             moviesList.value = await response.json();
             loading.value = false;
         };
 
-        const searchMovies = async (query) => {
+        const searchMovies = async () => {
+            const query = searchQuery.value;
             if (!query) {
                 await getMovies();
                 return;
             }
-            const response = await fetch(`${window.location.origin}/api/search.json?q=${encodeURIComponent(query)}`);
+            const response = await fetch(`${window.location.origin}/api/search.json?q=${encodeURIComponent(query)}&pageSize=${moviesQuantity.value}`);
             moviesList.value = await response.json();
         };
 
-        const onInput = (e) => {
+        const onInput = async (e) => {
             const value = e.target.value;
             if (!value.trim()) {
-                getMovies();
+                await getMovies();
             } else {
-                searchMovies(value.trim());
+                searchQuery.value = value;
+                await searchMovies();
             }
         };
 
         const openMovieModal = (movie) => {
             selectedMovie.value = movie;
+        };
+
+        const incrementMoviesQuantity = async () => {
+            const currentQuantity = Number(moviesQuantity.value) || 0;
+            const newQuantity = currentQuantity + 24;
+
+            moviesQuantity.value = newQuantity;
+
+            if (searchQuery.value && searchQuery.value.trim()) {
+                await searchMovies();
+            } else {
+                await getMovies();
+            }
         }
 
         onMounted(async () => {
@@ -80,9 +102,12 @@ export default {
         return {
             loading,
             moviesList,
+            moviesQuantity,
             onInput,
             selectedMovie,
-            openMovieModal
+            openMovieModal,
+            searchQuery,
+            incrementMoviesQuantity
         };
     }
 }
